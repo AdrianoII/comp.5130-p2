c9100cfbfef799578831c43c58fdccebeba75826
AdrianoII <adrianocorbelinoii@gmail.com>
2025-11-05 19:23:18 -0500

    Test neon get ex route

--- src/app/api/examples/route.ts
+++ src/app/api/examples/route.ts
@@ -1,5 +1,15 @@
+// app/actions.ts
+"use server";
+import { neon } from "@neondatabase/serverless";
+
+export async function getData() {
+    const sql = neon(process.env.DATABASE_URL);
+    const data = await sql`SELECT * FROM examples;`;
+    return data.toString();
+}
+
 export async function GET(request: Request) {
-  const response = await fetch('https://api.vercel.app/products');
-  const products = await response.json();
-  return Response.json(products);
+  // const response = await fetch('https://api.vercel.app/products');
+  // const products = await response.json();
+  return Response.json(getData());
 }
