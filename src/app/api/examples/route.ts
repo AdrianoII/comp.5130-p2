4db1bd19c0028b72684460206bb122637b945dc2
AdrianoII <adrianocorbelinoii@gmail.com>
2025-11-05 19:31:03 -0500

    Test route

--- src/app/api/examples/route.ts
+++ src/app/api/examples/route.ts
@@ -5,6 +5,7 @@ import { neon } from "@neondatabase/serverless";
 export async function getData() {
     const sql = neon(process.env.DATABASE_URL as string);
     const data = await sql`SELECT * FROM examples;`;
+    console.log(data);
     return data.toString();
 }
 
