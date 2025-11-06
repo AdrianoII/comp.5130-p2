463dba0cc8b9a37544e323fa52184d413789cefd
AdrianoII <adrianocorbelinoii@gmail.com>
2025-11-05 19:36:16 -0500

    Test route - 3

--- src/app/api/examples/route.ts
+++ src/app/api/examples/route.ts
@@ -12,5 +12,5 @@ export async function getData() {
 export async function GET(request: Request) {
   // const response = await fetch('https://api.vercel.app/products');
   // const products = await response.json();
-  return Response.json(getData());
+  return Response.json(await getData());
 }
