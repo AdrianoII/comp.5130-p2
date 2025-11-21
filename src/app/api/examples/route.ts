5fd16402e30bca579eabda7bfb33546739913ff7
AdrianoII <adrianocorbelinoii@gmail.com>
2025-11-20 21:49:22 -0500

    Auth url test

--- src/app/api/examples/route.ts
+++ src/app/api/examples/route.ts
@@ -10,7 +10,5 @@ export async function getData() {
 }
 
 export async function GET(request: Request) {
-  // const response = await fetch('https://api.vercel.app/products');
-  // const products = await response.json();
   return Response.json(await getData());
 }
--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,4 +1,5 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL:  process.env.BETTER_AUTH_URL || "http://localhost:3000",
+    // baseURL:  process.env.BETTER_AUTH_URL || "http://localhost:3000",
+    baseURL: "https://comp-5130-p2.vercel.app/",
 })
