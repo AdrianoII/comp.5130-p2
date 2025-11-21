dbd36dc54ddbcc7e53bf79aad6630879f583719e
AdrianoII <adrianocorbelinoii@gmail.com>
2025-11-20 21:52:00 -0500

    Auth url test - 2

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    // baseURL:  process.env.BETTER_AUTH_URL || "http://localhost:3000",
-    baseURL: "https://comp-5130-p2.vercel.app/",
+    baseURL:  process.env.NEXT_PUBLIC_URL || "http://localhost:3000",
+    // baseURL: "https://comp-5130-p2.vercel.app/",
 })
