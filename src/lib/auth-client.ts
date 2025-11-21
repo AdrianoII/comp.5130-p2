04218abeac610140bdb28a7ec86d462c471f1110
AdrianoII <adrianocorbelinoii@gmail.com>
2025-11-20 19:53:06 -0500

    Fix authentication on local environment

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,13 +1,4 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    // baseURL:  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
-    baseURL:  "https://comp-5130-p2.vercel.app/",
+    baseURL:  process.env.BETTER_AUTH_URL || "http://localhost:3000",
 })
-
-
-// const signIn = async () => {
-//     const data = await authClient.signIn.social({
-//         provider: "github"
-//     })
-// }
