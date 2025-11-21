4131dbff281d579df4f1e26a3e9fa5d7271d7ac3
AdrianoII <adrianocorbelinoii@gmail.com>
2025-11-20 21:03:47 -0500

    Update schema: - Rename tables to be on singular form; - Centralizes users to a singe table user; - Add role table;

--- schema.sql
+++ schema.sql
@@ -1,18 +1,60 @@
-CREATE TABLE users (
-    id           BIGSERIAL PRIMARY KEY,
-    email        TEXT NOT NULL UNIQUE,
-    password     TEXT NOT NULL,
-    role         TEXT NOT NULL CHECK (role IN ('admin', 'user')),
-    created_at   TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    updated_at   TIMESTAMPTZ NOT NULL DEFAULT NOW()
+create table "user" (
+    "id" text not null primary key,
+    "name" text not null,
+    "email" text not null unique,
+    "emailVerified" boolean not null,
+    "image" text,
+    "createdAt" timestamptz default CURRENT_TIMESTAMP not null,
+    "updatedAt" timestamptz default CURRENT_TIMESTAMP not null
 );
 
+create table "session" (
+    "id" text not null primary key,
+    "expiresAt" timestamptz not null,
+    "token" text not null unique,
+    "createdAt" timestamptz default CURRENT_TIMESTAMP not null,
+    "updatedAt" timestamptz not null,
+    "ipAddress" text, "userAgent" text,
+    "userId" text not null references "user" ("id") on delete cascade
+);
 
-CREATE TABLE examples (
-    id           BIGSERIAL PRIMARY KEY,
-    user_id      BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    title        TEXT NOT NULL,
-    code         TEXT NOT NULL,
-    created_at   TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    updated_at   TIMESTAMPTZ NOT NULL DEFAULT NOW()
+create table "account" (
+    "id" text not null primary key,
+    "accountId" text not null,
+    "providerId" text not null,
+    "userId" text not null references "user" ("id") on delete cascade,
+    "accessToken" text,
+    "refreshToken" text,
+    "idToken" text,
+    "accessTokenExpiresAt" timestamptz,
+    "refreshTokenExpiresAt" timestamptz,
+    "scope" text,
+    "password" text,
+    "createdAt" timestamptz default CURRENT_TIMESTAMP not null,
+    "updatedAt" timestamptz not null
+);
+
+create table "verification" (
+    "id" text not null primary key,
+    "identifier" text not null,
+    "value" text not null,
+    "expiresAt" timestamptz not null,
+    "createdAt" timestamptz default CURRENT_TIMESTAMP not null,
+    "updatedAt" timestamptz default CURRENT_TIMESTAMP not null
+);
+
+CREATE TABLE role (
+    id serial primary key,
+    user_id integer references users(id),
+    role varchar(100),
+    description text
+);
+
+CREATE TABLE example (
+    id bigserial primary key,
+    "userId" text not null references "user" ("id") on delete cascade,
+    title text not null,
+    code text not null,
+    created_at timestamptz not null default now(),
+    updated_at timestamptz not null default now()
 );
